297046af917f47ffa2cf21c45ce2a2c5fbf4c587
sahilshaikh78 <76987111+sahilshaikh78@users.noreply.github.com>
2021-01-05 11:15:19 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '^vzy1b_^sak&wh=i_-y=s_qkj#x!j_tlqrqwi1gx21w-rx+z24'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [127.0.0.1]
+ALLOWED_HOSTS = ['127.0.0.1','digital-product-finder.herokuapp.com']
 
 
 # Application definition
